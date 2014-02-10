7bcb773acd6e522b34037ef7ab870dcaf8777bc8
yuan <yuan@github.com>
2014-02-11 01:11:56 +0700

    penyelesaian konflik

--- src/medrecappx/Gui/FrmUtama.java
+++ src/medrecappx/Gui/FrmUtama.java
@@ -11,11 +11,8 @@
 package medrecappx.Gui;
 
 import java.awt.Dimension;
-<<<<<<< HEAD
 import medrecappx.Gui.Internal.FrmIntJaminan;
-=======
 import medrecappx.Gui.Internal.FrmIntObat;
->>>>>>> 69c69a3e25015a2e4a1aa8213a30dd757ccb70e9
 import medrecappx.Gui.Internal.FrmIntSpesialis;
 import medrecappx.Gui.Internal.FrmIntStaf;
 
