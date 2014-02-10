880032a221d25216e708d602db220d6c0971521d
beeboypralienka <beeboy_pralienka@rocketmail.com>
2014-02-10 23:32:45 +0700

    Environment awal MedRecAppX

---
+++ src/medrecappx/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medrecappx;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import medrecappx.Gui.FrmUtama;
+/**
+ *
+ * @author Fachrul Pralienka BM
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                FrmUtama fu = new FrmUtama();
+                fu.setExtendedState(fu.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+                fu.setVisible(true);
+            }
+        });
+    }
+}
